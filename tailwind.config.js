deec04bb104f7be9e46ed06a9aaec5fc386de9cd
Tharunprasath A S <20I250@psgtech.ac.in>
2021-12-15 19:27:16 +0530

    feat: added front page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,32 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
+    minWidth: {
+      320: "320px",
+    },
+    borderWidth: {
+      DEFAULT: "1px",
+      0: "0",
+      1: "1px",
+      1.5: "1.5px",
+      2: "2px",
+      3: "3px",
+      4: "4px",
+      6: "6px",
+      8: "8px",
+    },
     extend: {
+      inset: {
+        "40%": "40%",
+        "45%": "45%",
+      },
+      width: {
+        600: "37.5rem",
+        700: "43.75rem",
+      },
+      screens: {
+        desk: "700px",
+      },
       colors: {
         darkCyan: "hsl(176, 72%, 28%)",
         moderateCyan: "hsl(176, 50%, 47%)",
