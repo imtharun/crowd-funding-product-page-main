0d99e2883f0d8a11e838f4acb730e55099eb67e7
Tharunprasath A S <20I250@psgtech.ac.in>
2021-12-15 21:47:28 +0530

    feat: added modal and overlay

--- public/script.js
+++ public/script.js
@@ -1,5 +1,7 @@
 "use strict";
 
 const menu = document.querySelector(".hamburgerMenu");
+const overlay = document.querySelector(".overlay");
+const thankyouModal = document.querySelector(".thankyouModal");
 
 menu.addEventListener("click", function () {});
