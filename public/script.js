af3e331bb242f4f28e1e8c2230774b824e194c4c
Tharunprasath A S <20I250@psgtech.ac.in>
2021-12-16 15:44:26 +0530

    feat: added hamburger menu

--- public/script.js
+++ public/script.js
@@ -1,6 +1,9 @@
 "use strict";
 
 const menu = document.querySelector(".hamburgerMenu");
+const hamMenu = document.querySelector(".hamMenu");
+const hamSticks = document.querySelector(".hamSticks");
+const hamCross = document.querySelector(".hamCross");
 const overlay = document.querySelector(".overlay");
 const thankyouModal = document.querySelector(".thankyouModal");
 const bookmark = document.querySelector(".bookmark");
@@ -25,5 +28,20 @@ bookmark.addEventListener("click", function () {
     bookmarkCircle.setAttribute("fill", "#2F2F2F");
     bookmarkPath.setAttribute("fill", "#B1B1B1");
     bookmark.setAttribute("data-active", "0");
+  }
+});
+
+menu.addEventListener("click", function () {
+  console.log("clicked");
+  if (menu.dataset.active === "0") {
+    hamSticks.classList.add("hidden");
+    hamCross.classList.remove("hidden");
+    hamMenu.classList.remove("hidden");
+    menu.setAttribute("data-active", "1");
+  } else {
+    hamSticks.classList.remove("hidden");
+    hamCross.classList.add("hidden");
+    hamMenu.classList.add("hidden");
+    menu.setAttribute("data-active", "0");
   }
 });
